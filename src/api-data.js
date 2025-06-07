38d777f21bc048478bdea54b41a0ff9e972aeb4e
JAEdevtech <jae.ed.dev@gmail.com>
2025-06-08 01:07:01 +0200

    rm src to docs

--- src/api-data.js
+++
@@ -1,9 +0,0 @@
-export const weather = async (value) => {
-  if (value === null) {
-    sessionStorage.getItem("BrowserLocation");
-  }
-  const api_url = `/weather/${value}`;
-  const res = await fetch(api_url);
-  const data = await res.json();
-  return data;
-};
